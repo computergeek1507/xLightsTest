2b064fa8820de15dcfed8b8a59a923131ea18527
Scott Hanson <scooter_seh@yahoo.com>
2023-04-01 00:00:19 -0400

    added new

---
+++ screens/ControllerModelScreen.js
@@ -0,0 +1,26 @@
+import { Button, Input } from '@rneui/themed';
+import {
+  Keyboard,
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+  Image,
+} from "react-native";
+
+import React, { useEffect, useRef, useState } from "react";
+
+import { uploadController} from '../api/xLightsServer';
+import Toast from 'react-native-root-toast';
+
+const ControllerModelScreen = ({ route, navigation }) => {
+
+      
+   
+   
+
+};
+
+export default ControllerModelScreen;
---
+++ screens/TestingScreen.js
@@ -0,0 +1,53 @@
+import {
+    FlatList,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    TouchableHighlight,
+    View
+} from "react-native";
+
+import React, {useEffect,useState} from "react";
+
+import { getModels} from '../api/xLightsServer';
+
+const TestingScreen = ({ route, navigation }) => {
+    //const { history } = route.params;
+    //console.log("setting history with: ", route.params);
+
+
+    useEffect(() => {
+        navigation.setOptions({
+            headerLeft: () => (
+            <TouchableOpacity onPress={() => navigation.navigate("Start Screen")}>
+                <Text style={styles.headerButton}>Main Screen</Text>
+            </TouchableOpacity>
+            ),
+        });
+        });    
+
+
+    return (
+       <View>
+        </View>
+    );
+
+};
+
+const styles = StyleSheet.create({
+    headerButton: {
+        color: "#fff",
+        fontWeight: "bold",
+      },
+      historyLabelText: {
+          fontSize: 16,
+        padding: 2,
+      },
+      timeLabelText: {
+        color: "#808080",
+        fontSize: 8,
+        textAlign: 'right'
+      },
+});
+
+export default TestingScreen;
